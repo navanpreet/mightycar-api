2222cc54d16edb0542cc78bb4c23842d54707ef2
Navanpreet <navanprs@usc.edu>
2017-04-02 18:08:41 -0700

    updated user controller

--- services/api/user/UserController.py
+++ services/api/user/UserController.py
@@ -1,25 +1,40 @@
 from flask_restplus import Namespace, Resource, fields
+from flask import request
 from .UserModel import User as UserModel
 userModel = UserModel()
 import sys
 sys.path.append('..')
+from util.JSONEncoder import JSONEncoder 
+from response.respond import respond
 from util.generate_tokens import create_token
 import bcrypt
-import random, string
+import random, string, json
 
 
 api = Namespace('user', description="Everything concerning a user")
 
-user = api.model('user', {
-	'_id': fields.String(description='Unique user identifier'),
-	'username': fields.String( description='Username of a user'),
-	'password': fields.String( description='Password of a user'),
-	'first_name': fields.String( description='First name of a user'),
-	'last_name': fields.String( description='Last Name of a user'),
+
+'''Response Marshals'''
+
+user_output = api.model('user_output', {
+    'user_id': fields.String(description='User Id'),
+    'username': fields.String(description='Username of a user'),
+    'first_name': fields.String(description='First name of a user'),
+    'last_name': fields.String(description='Last Name of a user'),
+    'email': fields.String(description='Email Address'),
+    'date_of_birth': fields.DateTime(description='Date of birth of a user'),
+    'active': fields.Boolean(description='Is the user active'),
+    'creation_date': fields.DateTime(description='Date when the user was created'),
+    'email_verified': fields.Boolean(description='Is the email verified')
+    })
+
+user_input = api.model('user_input', {
+    'username': fields.String( description='Username of a user'),
+    'password': fields.String( description='Password of a user'),
+    'first_name': fields.String( description='First name of a user'),
+    'last_name': fields.String( description='Last Name of a user'),
     'email': fields.String( description='Email Address'),
-	'date_of_birth': fields.DateTime(description='Date of birth of a user'),
-	'active': fields.Boolean(description='Is the user active'),
-	'creation_date': fields.DateTime(description='Date when the user was created')
+    'date_of_birth': fields.DateTime(description='Date of birth of a user'),
     })
 
 authentication_token = api.model('authentication_token', {
@@ -28,47 +43,88 @@ authentication_token = api.model('authentication_token', {
     'salt': fields.String()
     })
 
-user_login = api.model('user_login', {
-    'username': fields.String( description='Username of a user'),
-    'password': fields.String( description='Password of a user'),    
+verification_token = api.model('verification_token', {
+    'user_id': fields.String(description="User id associated with the token"),
+    'verification_token': fields.String()
     })
 
+deletion_success = api.model('deletion_success', {
+    'message': fields.String(description="Status message")
+    })
+
+login_input = api.model('user_login_input', {
+    'username': fields.String( description='Username of a user'),
+    'password': fields.String( description='Password of a user')
+    })
+
+login_success = api.model('login_success', {
+    'user_id': fields.String( description='User id of a user'),
+    'authentication_token': fields.String( description='Authentication token for a user')
+    })
+
+logout_success = api.model('logout_success', {
+    'message': fields.String(description="Status message")
+    })
+
+logout_input = api.model('logout_input', {
+    'user_id': fields.String(description="User Id")
+    })
+
+verification_failure = {
+    'message': 'could not verify'
+    }
+
+
+''''''
+
+user_get = api.parser()
+user_get.add_argument('x-access-token', required=True, location='headers')   
+
 user_post = api.parser()
-user_post.add_argument(
-    	'data',
-    	type=user,
-    	required=True			
-    )
+user_post.add_argument('data', type=user_input)
 
 @api.route('/')
 class User(Resource):
 
     @api.doc('list all users')
-    @api.marshal_list_with(user)
+    @api.response(200, 'Successful', user_output)
+    @api.response(401, "Not authenticated")
+    @api.header('x-access-token', 'authentication token', required=True)
+    @api.marshal_list_with(user_output)
     def get(self):
     	'''List all users'''
-    	user = UserModel.get(self)
-        return user
+        args = user_get.parse_args()
+        token = args['x-access-token']
+        search_token = userModel.searchAuthenticationToken(token)
+        if(search_token==False):
+            api.abort(401, "Not authenticated")
+    	user_output = userModel.get()
+
+        return user_output
 
 
-    @api.expect(user)
-    @api.response(200, 'User added')
+    @api.expect(user_input)
+    @api.response(200, 'User added', login_success)
     @api.response(500, 'Cannot add new user')
-    @api.doc('post_user')
-    @api.marshal_with(user)
+    @api.doc('Add a new user')
     def post(self):
         '''Add a New User'''
+
         args = user_post.parse_args()
         data = args['data']
-        del data['_id']
         data['user_verified'] = False
-        hashed_password = bcrypt.hashpw(str(data['password']), bcrypt.gensalt())    
+        data['email_verified'] = False
+        if '_id' in data:
+            del data['_id']
+        hashed_password = bcrypt.hashpw(str(data['password']).encode('utf8'), bcrypt.gensalt())    
         data['password'] = hashed_password
         newUser = userModel.createOne(data)
 
-        if (newUser == {}):
-            api.abort(500, 'Cannot create new user')
+        if isinstance(newUser, dict) and 'error' in newUser.keys():
+            api.abort(500, newUser['error'])
+        
         newUserObjectId = newUser.inserted_id
+        
         key = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])
         token = create_token(str(newUserObjectId), key)
         tokenObj = {
@@ -77,7 +133,7 @@ class User(Resource):
             'salt': key
         }
         insertedToken = userModel.createAuthenticationToken(tokenObj);
-        createdUser = userModel.getByObjectId(newUserObjectId)
+        
         # Create verification token and send email to verify user
         verificationToken = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])
         verificationTokenObj = {
@@ -86,95 +142,234 @@ class User(Resource):
         }
         insertedVerificationToken = userModel.createEmailVerficationToken(verificationTokenObj)
 
-        return createdUser, token
+        response = {'user_id': str(newUserObjectId), 'authentication_token': token}
+            
+        return response
 
 user_put = api.parser()
-user_put.add_argument(
-    'data',
-    type=user,
-    required=True		
-)
+user_put.add_argument('x-access-token', required=True, location='headers')
+user_put.add_argument('data', type=user_input)
+
+user_delete = api.parser()
+user_delete.add_argument('x-access-token', required=True, location='headers')
 
 @api.route('/<id>')
 class UserById(Resource):
 
-    @api.param('id', 'User Id')
+
     @api.response(404, 'User not found')
+    @api.response(401, "Not authenticated")
+    @api.response(200, 'Successful', user_output)
     @api.doc('Get user by id')
-    @api.marshal_with(user)
+    @api.header('x-access-token', 'authentication token', required=True)
+    @api.marshal_with(user_output)
     def get(self, id):
     	'''Get a user by id'''
-    	user = UserModel.getOne(self, id)
-    	return user
+        token = user_get.parse_args()['x-access-token']
+        search_token = userModel.searchAuthenticationToken(token)
+        if(search_token==False):
+            api.abort(401, "Not authenticated")
+    	user_output = userModel.getOne(id)
+        return user_output
 
-    @api.expect(user)
-    @api.param('id', 'User id')
-    @api.response(200, 'User added')
-    @api.response(500, 'Cannot update new user')
-    @api.doc('update user')
-    @api.marshal_with(user)
-    def put(self):
+    @api.expect(user_input)
+    @api.response(200, 'User added', user_output)
+    @api.response(500, 'Cannot update user')
+    @api.response(401, "Not authenticated")
+    @api.header('x-access-token', 'authentication token', required=True)
+    @api.doc('update a user')
+    @api.marshal_with(user_output)
+    def put(self, id):
         '''Update an existing user'''
-        args = user_put.parse_args()
-        data = args['data']
-        del data['_id']
-        updatedUser = userModel.updateOne(self, id, user)
+        token = user_put.parse_args()['x-access-token']
+        data = user_put.parse_args()['data']
+        search_token = userModel.searchAuthenticationToken(token)
+        if(search_token==False):
+            api.abort(401, "Not authenticated")
+
+        updatedUser = userModel.updateOne(id, str(data))
 
         if(updatedUser.matched_count == 0):
-        	api.abort(404, 'Vehicle Not found')
+        	api.abort(404, 'User Not found')
 
-        newDetails = userModel.getOne(self, id)
+        user_output = userModel.getOne(id)
+        return user_output
 
-        return newDetails
-
-    @api.param('id', 'User id')
     @api.response(404, 'User not found')
+    @api.response(200, 'User deleted', deletion_success)
+    @api.header('x-access-token', 'authentication token', required=True)
     @api.doc('delete user')
     def delete(self, id):
-    	'''Search a user by id'''
-    	userDeleteCount = userModel.delete(self, id).deleted_count
+    	'''Delete a user by id'''
+        token = user_delete.parse_args()['x-access-token']
+        search_token = userModel.searchAuthenticationToken(token)
+        if(search_token==False):
+            api.abort(401, "Not authenticated")
+
+    	userDeleteCount = userModel.delete(id)
     	if(userDeleteCount == 0):
     		api.abort(404, 'User not found')
-    	return {'code': 200, 'message': 'user successfully deleted'}
 
-user_search = api.parser()
-user_search.add_argument(
-	'data',
-	type=dict
-)
-
+    	return {'message': 'Successfully deleted user'}
 
 user_login_post = api.parser()
-user_login_post.add_argument(
-        'data',
-        type=user_login
-    )
+user_login_post.add_argument('username', required=True, location='form')
+user_login_post.add_argument('password', required=True, location='form')
 
 @api.route('/login')
 class Login(Resource):
 
-    @api.expect(user_login)
-    @api.response(200, 'Login Successful')
-    @api.response(401, 'Login Failed')
-    @api.doc('user_login')
-    @api.marshal_with(user_login)
+    @api.expect(user_login_post)
+    @api.response(200, 'Login Successful', login_success)
+    @api.response(401, 'username or password does not match')
+    @api.doc('user login')
     def post(self):
         '''Accepts a username and password to create a new token'''
-        args = user_login.parse_args()
-        data = args['data']
-        del data['_id']
-        
-        userData = userModel.getByUsername(self, data['username'])
-        if bcrypt.checkpw(data['password'], userData['password']):
+        args = user_login_post.parse_args()
+        userData = userModel.getByUsername(args['username'])
+        if bcrypt.checkpw(str(args['password']), userData['password'].encode('utf8')):
             key = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(32)])
-            token = create_token(str(newUserObjectId), key)
+            token = create_token(str(userData['_id']), key)
             tokenObj = {
                 'user_id': userData['_id'],
                 'authentication_token': token,
                 'salt': key
             }
-            insertedToken = userModel.createAuthenticationToken(tokenObj);
-            return userData['_id'], token
+            insertedToken = userModel.createAuthenticationToken(tokenObj)
+            response = {'user_id': str(userData['_id']), 'authentication_token': token}
+            return response 
+        else:
+            api.abort(401, login_failure)
+
+
+logout_post = api.parser()
+logout_post.add_argument('user_id', required=True)
+logout_post.add_argument('x-access-token', required=True, location='headers')
+
+@api.route('/logout')
+class Logout(Resource):
+
+
+    @api.response(200, 'Logout Successful', logout_success)
+    @api.response(404, 'Token not found')
+    @api.expect(logout_input)
+    @api.header('x-access-token', 'authentication token', required=True)
+    @api.doc('user logout')
+    def post(self):
+        token = logout_post.parse_args()['x-access-token']
+        user_id = logout_post.parse_args()['user_id']
+        killedToken = userModel.killToken(token)
+        return logout_success
+
+verify_get = api.parser()
+verify_get.add_argument('token', location='args')
+
+@api.route('/verify')
+class Verify(Resource):
+ 
+    @api.expect(verify_get)
+    @api.response(200, 'Email verified')
+    @api.response(404, 'Token not found')
+    def get(self):
+        '''Verify email'''
+        token = verify_get.parse_args()['token']
+        reset = verify_get.parse_args()['reset']
+        verificationTokenLookup = userModel.searchEmailVerificationToken(token)
+        if verificationTokenLookup == {}:
+            return verification_failure
+
+        userData = userModel.getOne(verificationTokenLookup['user_id'])
+        userData['email_verified'] = True
+
+        updatedUserData = userModel.updateOne(verificationTokenLookup['user_id'], userData)
+        if updatedUserData == {}:
+            return verification_failure
+
+        deleteVerificationToken = userModel.deleteEmailVerificationToken(verificationTokenLookup['verification_token'])
+        if deleteVerificationToken == {}:
+            return verification_failure
+
+        return {'message': 'Verified'}
+
+forgot_post = api.parser()
+forgot_post.add_argument('x-access-token', required=True, location='headers')
+forgot_post.add_argument('user_id', required=True, location='form')
+forgot_post.add_argument('email', required=True, location='form')
+
+@api.route('/forgot')
+class Forgot(Resource):
+
+    @api.expect(forgot_post)
+    @api.response(200, 'Email successfully sent')
+    @api.response(404, 'Email not found')
+    @api.response(401, 'Not authenticated')
+    def post(self):
+        '''Send email to reset password'''
+        token = forgot_post.parse_args()['x-access-token']
+        user_id = forgot_post.parse_args()['user_id']
+        email = forgot_post.parse_args()['email']
+
+        search_token = userModel.searchAuthenticationToken(token)
+        
+        if search_token==False:
+            api.abort(401, "Not authenticated")
+        if search_token['user_id'] != user_id:
+            api.abort(401, "Not authenticated")
+
+        user_output = userModel.getOne(user_id)
+
+        if email == user_output['email']:
+            tempPassword = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(8)])
+            tempPasswordObj = {
+                'user_id': user_id,
+                'verification_token': tempPasswordObj
+            }
+            insertedVerificationToken = userModel.createEmailVerficationToken(verificationTokenObj)
+            #send email with temp password
 
         else:
-            return{'status': 401, 'message': 'username or password does not match'}
+            api.abort(404, "Email not found")
+
+reset_post = api.parser()
+reset_post.add_argument('temp_password', required=True, location='form')
+reset_post.add_argument('new_password', required=True, location='form')
+
+@api.route('/reset')
+class Reset(Resource):
+
+    @api.response(200, 'Password successfully sent')
+    @api.response(404, 'User not found')
+    def post(self):
+        '''Reset password'''
+        temp_password = reset_post.parse_args()['temp_password']
+        new_password = reset_post.parse_args()['new_password']
+        tempPasswordLookup = userModel.searchEmailVerificationToken(temp_password)
+        if tempPasswordLookup == {}:
+            return verification_failure
+
+        hashedPassword = bcrypt.hashpw(new_password.encode('utf8'), bcrypt.gensalt())  
+        
+        userData = userModel.getOne(tempPasswordObj['user_id'])
+
+        if userData == {}:
+            api.abort(404, 'User Not found')
+
+        userData['password'] = hashedPassword
+
+        updatedUserData = userModel.updateOne(userData['_id'], userData)
+
+        if(updatedUser.matched_count == 0):
+            api.abort(404, 'User Not found')
+
+        return {'message': 'Password successfully reset'}
+
+
+
+
+
+
+
+
+
+
+
--- services/api/user/UserModel.py
+++ services/api/user/UserModel.py
@@ -6,9 +6,10 @@ from flask_pymongo import *
 import json
 import sys
 sys.path.append('..')
-from util import JSONEncoder as JSONEncoder
+from util.JSONEncoder import JSONEncoder 
 import json
 from config import mongo
+import ast
 
 app = Flask(__name__)
 
@@ -25,7 +26,7 @@ class User():
 		if userResult == None:
 			return {}
 
-		return userResult
+		return json.loads(JSONEncoder().encode(userResult))
 
 
 	def getByObjectId(self, objectId):
@@ -40,7 +41,7 @@ class User():
 		if users == None:
 			return []
 
-		return list(users)
+		return json.loads(JSONEncoder().encode(list(users)))
 
 	def getByUsername(self, username):
 		userResult = mongo.db.user.find_one({'username': username})
@@ -53,43 +54,70 @@ class User():
 	def createOne(self, userData):
 		
 		checkUnique = mongo.db.user.find({'email': userData['email']}).limit(1)
-		if(list(checkUnique) != []):
-			return "email already exists"
+		if list(checkUnique) != []:
+			return {"error": "email already exists"}
 
 		checkUnique = mongo.db.user.find({'username': userData['username']}).limit(1)
-		if(list(checkUnique) != []):
-			return "username already exists"	
+		if list(checkUnique) != []:
+			return {"error": "username already exists"}	
 
 		newUser = mongo.db.user.insert_one(userData)
-		if (newUser == None):
+		if newUser == None:
 			return {}
 		return newUser
 
 	def createAuthenticationToken(self, tokenData):
 		newToken = mongo.db.authentication_token.insert_one(tokenData)
-		if(newToken == None):
+		if newToken == None:
 			return {}
 		return newToken
 
+	def searchAuthenticationToken(self, token):
+		result = mongo.db.authentication_token.find_one({'authentication_token': token})
+		if result == None:
+			return {}
+		return result
+
+	def killToken(self, token):
+		deletedToken = mongo.db.authentication_token.delete_one({'authentication_token': token})
+		if deletedToken == None:
+			return False
+		return True
+
 	def createEmailVerficationToken(self, verificationTokenData):
 		newToken = mongo.db.verification_token.insert_one(verificationTokenData)
-		if(newToken == None):
+		if newToken == None:
 			return {}
 		return newToken	
 
+	def searchEmailVerificationToken(self, token):
+		result = mongo.db.verification_token.find_one({'verification_token': token})
+		if result == None:
+			return {}
+
+		return json.loads(JSONEncoder().encode(dict(result)))
+
+	def deleteEmailVerificationToken(self, token):
+		result = mongo.db.verification_token.delete_one({'verification_token': token})
+		if result == None:
+			return False
+		return True
+
 	def updateOne(self, id, userData):
-		updatedUser = mongo.db.user.update_one({'_id':ObjectId(id)}, {"$set": userData})
-		if (updatedUser == None):
+		res = ast.literal_eval(userData)
+		updatedUser = mongo.db.user.update_one({'_id':ObjectId(id)}, {"$set": res})
+		if updatedUser == None:
 			return {}
 		return updatedUser
 
 	def delete(self, id):
-		deletedUser = mongo.db.user.delete_one({'_id': ObjectId(id)})
-		return deletedUser						
+		deletedUserCount = mongo.db.user.delete_one({'_id': ObjectId(id)})
+		print("++++++  ", deletedUserCount.deleted_count)	
+		return deletedUserCount.deleted_count				
  
 	def search(self, criteria):
-		users = mongo.db.vehicle.find(criteria)
-		if (users == None):
+		users = mongo.db.user.find(criteria)
+		if users == None:
 			return []
- 
+
 		return json.loads(JSONEncoder().encode(list(users)))		
